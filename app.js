638feb2ee7eb066a2c6482e8b71a53627d174a77
Henry Doan <henrydoan96@gmail.com>
2022-10-11 19:10:21 -0600

    webpack notes

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Loading...");
+
+document.write("Well, hello World with Webpack!");
+
+// run $ webpack ./app.js bundle.js
+// first arg is the input file on what webpack runs, the second arg 
+// is where we are putting the output, will create the second arg file  
+
+// add watch to watch for some changes and will rebundle and do a production build
+// run $webpack ./app.js bundle.js --watch
